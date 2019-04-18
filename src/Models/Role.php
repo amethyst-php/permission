f6647e4c35a093716fd66fe4d6863bbb29a6bcca
railken <railken@protonmail.ch>
2019-04-18 12:33:27 +0200

    add relation to role

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -5,6 +5,7 @@ namespace Railken\Amethyst\Models;
 use Spatie\Permission\Models\Role as Model;
 use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model implements EntityContract
 {
@@ -19,5 +20,13 @@ class Role extends Model implements EntityContract
     {
         $this->ini('amethyst.permission.data.role');
         parent::__construct($attributes);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function hasPermissions(): HasMany
+    {
+        return $this->hasMany(RoleHasPermission::class);
     }
 }
