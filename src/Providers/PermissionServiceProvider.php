c42d478f58c0db1f57b3b5f21977b01c06c3d2b8
railken <railken@protonmail.ch>
2019-11-12 01:20:52 +0100

    remove OwnablePermissionObserver

--- src/Providers/PermissionServiceProvider.php
+++ src/Providers/PermissionServiceProvider.php
@@ -6,7 +6,6 @@ use Amethyst\Common\CommonServiceProvider;
 use Amethyst\Console\Commands;
 use Amethyst\Models\Ownable;
 use Amethyst\Models\Permission;
-use Amethyst\Observers\OwnablePermissionObserver;
 use Amethyst\Observers\PermissionObserver;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
@@ -39,7 +38,6 @@ class PermissionServiceProvider extends CommonServiceProvider
         parent::boot();
 
         Permission::observe(PermissionObserver::class);
-        // Ownable::observe(OwnablePermissionObserver::class);
 
         if (Schema::hasTable(Config::get('amethyst.permission.data.permission.table'))) {
             app('amethyst.permission')->boot();
